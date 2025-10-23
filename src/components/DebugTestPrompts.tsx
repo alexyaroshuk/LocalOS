e022127e44427fcf9f6b827c60790c6d057023eb
Alex Yaroshuk <alexander_4288@Yahoo.com>
2025-10-24 00:18:21 +0800

    update errors.txt and DebugTestPrompts.tsx

--- src/components/DebugTestPrompts.tsx
+++ src/components/DebugTestPrompts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
 
 interface DebugTestPromptsProps {
   onPromptSelect: (prompt: string) => void;
