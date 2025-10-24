80d9b2382f11c96c088f70cdfcd7c22345136575
Alex Yaroshuk <alexander_4288@Yahoo.com>
2025-10-24 16:32:50 +0800

    update 6 files

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// Polyfills for AI SDK - must be imported before anything else
+import 'web-streams-polyfill/polyfill';
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
